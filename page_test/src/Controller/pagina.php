6b41b31988cc988680e92d4d4112a62c6457604a
drmfraterni <44780833+drmfraterni@users.noreply.github.com>
2018-12-11 01:05:05 +0100

    Add files via upload

--- page_test/src/Controller/pagina.php
+++ page_test/src/Controller/pagina.php
@@ -21,7 +21,68 @@ class pagina extends ControllerBase {
       '#markup' => $this->t('Página desde otro controlador'),
     ];
   }
+  public function verpagina($idpagina = null) {
+    return [
+      '#type' => 'markup',
+      '#markup' =>'El parámetro recibido es: '.$idpagina,
+    ];
+  }
+  public function verpaginacustom($custom_arg) {
+    return [
+      '#type' => 'markup',
+      '#markup' =>'El parámetro recibido es: '.$custom_arg,
+    ];
+  }
+  public function varias() {
 
+    $contenido=array();
 
+    $contenido['linea1']=array (
+      '#markup' =>'<strong>Linea 1</strong></br>',
+    );
+    $contenido['linea2']=array (
+      '#markup' =>'<i>Linea 2</i></br></br>',
+    );
+
+    $contenido['linea3']=array (
+      '#markup' =>'<i>Linea 3</i></br></br>',
+    );
+
+    return $contenido;
+
+  }
+
+  public function form() {
+
+    $contenido=array();
+
+    $contenido['linea1']=array (
+      '#markup' =>'<strong>Linea 1</strong></br>',
+    );
+    $contenido['linea2']=array (
+      '#markup' =>'<i>Linea 2</i></br></br>',
+    );
+
+    $contenido['linea3']=array (
+      '#markup' =>'<i>Linea 3</i></br></br>',
+    );
+
+    $contenido['linea4']=\Drupal::formBuilder()->getForm('Drupal\form_example\Form\addform');
+
+    return $contenido;
+
+  }
+
+  public function template() {
+
+    $form=\Drupal::formBuilder()->getForm('Drupal\form_example\Form\addform');
+
+      return [
+        '#theme' => 'my_template',
+        '#mi_variable' => $this->t('Esta es la variable'),
+        '#form' => $form,
+      ];
+
+    }
 
 }
